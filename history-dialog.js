5fde37c010c57e147220ec143ff36c901043c396
Rob van den Berg <rghvdberg@gmail.com>
2026-08-08 12:10:58 +0200

    Fix: Image preview height constraint
    - Change content_fit from COVER to SCALE_DOWN to show full image
    - Add max_height_request: 400 to limit vertical space
    - Images now visible in full with max 400px height

--- history-dialog.js
+++ history-dialog.js
@@ -610,7 +610,8 @@ app.connect('activate', () => {
             valign: Gtk.Align.START,
             hexpand: true,
             width_request: 400,
-            content_fit: Gtk.ContentFit.COVER,
+            max_height_request: 400,
+            content_fit: Gtk.ContentFit.SCALE_DOWN,
             css_classes: ['ntfy-image-preview'],
         });
         
